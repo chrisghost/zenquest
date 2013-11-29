4026313d1d3ba08edcb4cb87ed20101944e2b21b
Adrien Maillol <ama@zenexity.com>
2013-11-29 14:41:50 +0100

    Fix index URL

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,10 +9,10 @@ import services._
 
 object Application extends Controller {
 
-  def index(menu: String) = Action { implicit request =>
+  def index(menu: Option[String]) = Action { implicit request =>
     request.session.get("gid") match {
       case Some(id) => GameDao.byId(id.toLong).map { game =>
-        val choices = MenuDao.byId(menu)
+        val choices = MenuDao.byId(menu.getOrElse(""))
 
         game.state.position match {
           case "start" => Ok(views.html.start(game, choices))
@@ -26,7 +26,7 @@ object Application extends Controller {
 
   def create = Action {
     val nGame = GameDao.create(Game())
-    Redirect(routes.Application.index(""))
+    Redirect(routes.Application.index(None))
       .withSession(
         Session(
           Map(
@@ -44,7 +44,7 @@ object Application extends Controller {
       else
         GameService.goTo(id.toLong, dest)
 
-      Redirect(routes.Application.index(""))
+      Redirect(routes.Application.index(None))
     }.getOrElse( Redirect(routes.Application.create).withSession() )
   }
 }
